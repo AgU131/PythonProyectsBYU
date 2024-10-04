95ede94e39a771b30e61e6efffb288aa6e5ab541
Agustin <82238717+AgU131@users.noreply.github.com>
2024-10-03 22:24:03 -0300

    Update adventure_gameW3_CSE110.py

--- W3/adventure_gameW3_CSE110.py
+++ W3/adventure_gameW3_CSE110.py
@@ -12,7 +12,7 @@ if item1.lower() == 'match' :
     print('You pick up the match and strike it, and for an instant, the forest around you is illuminated. You see a large grizzly bear, and then the match burns out. Do you want to RUN, or HIDE behind a tree?')
     action1 = input(str())
         # if  :
-
+#i need to use my creativity here and ad up to 3 questions and responses
         # else:
 elif item1.lower() == 'flashlight' :
     print('You pick up the flashlight and turn it on. You see the pathway lit up in front of you, but you thought you also heard something off to the side. Do you want to FOLLOW the path or LOOK in the trees for the thing that made the noise?')
