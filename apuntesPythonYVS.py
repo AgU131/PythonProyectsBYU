5c7128e88bd80daed8dad1a9c12eec582082392e
Agustin <82238717+AgU131@users.noreply.github.com>
2024-10-21 12:40:45 -0300

    Add files via upload

--- apuntesPythonYVS.py
+++ apuntesPythonYVS.py
@@ -8,10 +8,20 @@ comentarios
 # Ctrl +k + c  --> comment
 
 
-"""
+"""     Atajos Terminal y VS Code
 ctrl + C (en la terminal de Vs) ---> Corta el programa, lo termina
 """
 
 
+"""     Apuntes Python
+Listas: 
+    decir esto:      clients = []      es lo mismo que decir esto:      clients = list()
+
+Numeros
+    variable:.2f eso me da la variable que es un float pero solo 2 num despues de la coma
+
+Operaciones
+    algo += esto    ==  algo = algo + esto
+"""
 
 
